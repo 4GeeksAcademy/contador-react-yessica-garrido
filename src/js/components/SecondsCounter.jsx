466263fabfd2535f499419ea9ce330878dbf7be1
yessigarridowork-blip <yessigarrido.work@gmail.com>
2025-11-04 18:09:14 +0000

    micontador

---
+++ src/js/components/SecondsCounter.jsx
@@ -0,0 +1,36 @@
+import React, { useState, useEffect } from "react";
+
+const SecondsCounter = (props) => {
+  const [seconds, setSeconds] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setSeconds((preSeconds) => preSeconds + 1);
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const segundos = seconds % 60;
+  const minutos = Math.floor(seconds / 60) % 60;
+  const horas = Math.floor(seconds / 3600);
+
+  return (
+    <div id="totalcontador">
+      <div id="contador" className=" bg-dark text-white text-center mt-5">
+        <h1>⏱️</h1>
+        <div className="numbers text-center">
+          {String(horas).padStart(2, "0")}:
+          {String(minutos).padStart(2, "0")}:
+          {String(segundos).padStart(2, "0")}
+        </div>
+        </div>
+
+      
+    </div>
+  );
+};
+
+export default SecondsCounter;
+
+
--- src/js/main.jsx
+++ src/js/main.jsx
@@ -8,11 +8,13 @@ import "bootstrap"
 // index.css'
 import '../styles/index.css'
 
+
 // components
-import Home from './components/Home';
+import SecondsCounter from './components/SecondsCounter';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <SecondsCounter/>
   </React.StrictMode>,
 )
